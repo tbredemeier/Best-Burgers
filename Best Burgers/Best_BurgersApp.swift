303d3464a66216c7255fa38acb585a568257ca79
Tom Bredemeier <tbredemeier@barrington220.org>
2022-01-20 10:17:29 -0600

    Initial Commit

---
+++ Best Burgers/Best_BurgersApp.swift
@@ -0,0 +1,17 @@
+//
+//  Best_BurgersApp.swift
+//  Best Burgers
+//
+//  Created by Tom Bredemeier on 1/20/22.
+//
+
+import SwiftUI
+
+@main
+struct Best_BurgersApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Best Burgers/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Best Burgers
+//
+//  Created by Tom Bredemeier on 1/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
